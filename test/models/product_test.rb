b44a0dc3e3055a2dce239dc18c8deaca5d3d2846
jasonwooCH <smwlks@hotmail.com>
2015-10-14 22:05:50 -0700

    added fixture data

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,6 +4,8 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  fixtures :products
   
   test "product attributes must not be empty" do
     product = Product.new
@@ -50,6 +52,15 @@ class ProductTest < ActiveSupport::TestCase
     bad.each do |name|
       assert new_product(name).invalid?, "#{name} shouldnt' be valid"
     end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title:       products(:ruby).title,
+    	      	          description: "yyy",
+			  price:       1,
+			  image_url:   "fred.gif")
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join('; ')
   end
 
 end
